236bc2578b2486e3ca7b1cf844f2333da96afb22
ROJAS Juan Ana Karina <rojasjuananakarina@gmail.com>
2022-06-24 00:06:25 -0500

    creación del archivo pokemonList y uso de useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import axios from "axios";
+import PokemonList from "./PokemonList";
 
 //`https://pokeapi.co/api/v2/pokemon
 function App() {
   const [currentPageUrl, setCurrentPageUrl] = useState ('https://pokeapi.co/api/v2/pokemon')
-  const [pokemon, setPokemon] = useState ([]);
-  const [nextBtnUrl, setNextBtnUrl] = useState ([]);
-  const [previousBtnUrl, setPreviousBtnUrl] = useState ([]);
+  const [pokemon, setPokemon] = useState ();
+  const [nextBtnUrl, setNextBtnUrl] = useState ();
+  const [previousBtnUrl, setPreviousBtnUrl] = useState ();
   
   useEffect(() => {
     axios.get(currentPageUrl)
     .then(res => {
       setNextBtnUrl(res.data.next);
-      setPreviousBtnUrl(res.data.previous);
-      setPokemon(res.data.results);
+      setPreviousBtnUrl(res.data.previous)
+      setPokemon(res.data.results)
     })
-  },[])
+  },[currentPageUrl])
+
+  const pagination = (e) => {
+    if(e.target.textContent === "next") {
+      setCurrentPageUrl(nextBtnUrl)
+    }
+    if(e.target.textContent === "previous") {
+      setCurrentPageUrl(previousBtnUrl)
+}
 
   return (
     <div className="App">
       {pokemon.map(pokemon =>
-        <pokemonList pokemon = {pokemon}/>
+        <PokemonList pokemon = {pokemon}/>
         )}
         <div className ="page-buttons">
           <button onClick={pagination} className="previous">previos</button>
           <button onClick={pagination} className="next">next</button>
         </div>
-      
     </div>
   );
 }
